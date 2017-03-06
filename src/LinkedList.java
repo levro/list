d8fa26e47005626897116ffd6667ffb701073aa8
levro <levro@ukr.net>
2017-03-06 13:15:54 +0200

    LinkedList added

---
+++ src/LinkedList.java
@@ -0,0 +1,30 @@
+public class LinkedList {
+	public LinkedList() {
+		//PUT YOUR CODE HERE
+		//PUT YOUR CODE HERE
+	}
+
+	public void add(Integer data) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public Integer get(int index) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+		return 1;
+	}
+
+	public boolean delete(int index) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+		boolean a = true;
+		return a;
+	}
+
+	public int size() {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+		return 1;
+	}
+}   
